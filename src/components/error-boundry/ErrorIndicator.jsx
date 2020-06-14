eb8e31d73c543c2ed524b3a9e82619a79abaea82
samerlagha <samer_la@mail.ru>
2020-06-14 15:38:21 +0300

    commit

--- src/components/error-boundry/ErrorIndicator.jsx
+++ src/components/error-boundry/ErrorIndicator.jsx
@@ -5,7 +5,7 @@ const ErrorIndicator = () => {
   return (
     <div className="error-indicator">
       <span className="boom">Error!!!</span>
-      <span>but we already sent to fix it</span>/span>
+      <span>but we already sent to fix it</span>
       
     </div>
   );
